5e9699e39367da60af20c14100b802145bfc0148
Bilal <yyybilal@gmail.com>
2025-12-23 20:38:14 +0100

    Adds core SwiftyTailwind functionality
    Introduces the core functionality for downloading and executing Tailwind CSS within Swift projects.
    
    Provides an API to initialize Tailwind configurations and run the Tailwind CLI with customizable options for input, output, and processing.

--- Sources/SwiftyTailwind/SwiftyTailwind.swift
+++ Sources/SwiftyTailwind/SwiftyTailwind.swift
@@ -8,58 +8,49 @@ public class SwiftyTailwind {
     private let directory: AbsolutePath
     private let downloader: Downloading
     private let executor: Executing
-    
-    /**
-     Default initializer.
-     - Parameters:
-     - version: The version of Tailwind to use. You can specify a fixed version or use the latest one.
-     - directory: The directory where the executables will be downloaded. When not provided, it defaults to the system's default temporary directory.
-     */
+
+    /// Default initializer.
+    /// - Parameters:
+    /// - version: The version of Tailwind to use. You can specify a fixed version or use the latest one.
+    /// - directory: The directory where the executables will be downloaded.
+    /// When not provided, it defaults to the system's default temporary directory.
     public convenience init(version: TailwindVersion = .latest, directory: AbsolutePath) {
         self.init(version: version, directory: directory, downloader: Downloader(), executor: Executor())
     }
-    
-    /**
-     Default initializer.
-     - Parameters:
-     - version: The version of Tailwind to use. You can specify a fixed version or use the latest one.
-     */
+
+     /// Default initializer.
+     /// - Parameters:
+     /// - version: The version of Tailwind to use. You can specify a fixed version or use the latest one.
     public convenience init(version: TailwindVersion = .latest) {
         self.init(version: version, directory: Downloader.defaultDownloadDirectory(), downloader: Downloader(), executor: Executor())
     }
-    
+
     init(version: TailwindVersion,
          directory: AbsolutePath,
          downloader: Downloading,
-         executor: Executing)
-    {
+         executor: Executing) {
         self.version = version
         self.directory = directory
         self.downloader = downloader
         self.executor = executor
     }
-    
-    /**
-     It runs the `init` command to create a Tailwind configuration file (i.e., `tailwind.config.js`)
-     - Parameters:
-     - directory: The directory in which the Tailwind configuration will be created. When not passed, it defaults to the working directory from where the process is running.
-     - options: A set of ``SwiftyTailwind.InitializeOption`` options to customize the initialization.
-     */
+
+    /// It runs the `init` command to create a Tailwind configuration file (i.e., `tailwind.config.js`)
+    /// - Parameters:
+    /// - directory: The directory in which the Tailwind configuration will be created. When not passed, it defaults to the working directory from where the process is running.
+    /// - options: A set of ``SwiftyTailwind.InitializeOption`` options to customize the initialization.
     public func initialize(directory: AbsolutePath = localFileSystem.currentWorkingDirectory!,
-                           options: InitializeOption...) async throws
-    {
+                           options: InitializeOption...) async throws {
         var arguments = ["init"]
         arguments.append(contentsOf: Set(options).executableFlags)
         let executablePath = try await download()
         try await executor.run(executablePath: executablePath, directory: directory, arguments: arguments)
     }
-    
-    /**
-     It runs the main Tailwind command.
-     - Parameters:
-     - directory: The directory from where to run the command. When not passed, it defaults to the working directory from where the process is running.
-     - options: A set of ``SwiftyTailwind.RunOption`` options to customize the execution.
-     */
+
+    /// It runs the main Tailwind command.
+    /// - Parameters:
+    /// - directory: The directory from where to run the command. When not passed, it defaults to the working directory from where the process is running.
+    /// - options: A set of ``SwiftyTailwind.RunOption`` options to customize the execution.
     public func run(input: AbsolutePath,
                     output: AbsolutePath,
                     directory: AbsolutePath = localFileSystem.currentWorkingDirectory!,
@@ -73,28 +64,21 @@ public class SwiftyTailwind {
         let executablePath = try await download()
         try await executor.run(executablePath: executablePath, directory: directory, arguments: arguments)
     }
-    
-    /**
-     Downloads the Tailwind portable executable
-     */
+    /// Downloads the Tailwind portable executable
     private func download() async throws -> AbsolutePath {
         try await downloader.download(version: version, directory: directory, numRetries: 0)
     }
 }
 
 extension Set where Element == SwiftyTailwind.InitializeOption {
-    /**
-     Returns the flags to pass to the Tailwind CLI when invoking the `init` command.
-     */
+    /// Returns the flags to pass to the Tailwind CLI when invoking the `init` command.
     var executableFlags: [String] {
         return self.map(\.flag)
     }
 }
 
 extension Set where Element == SwiftyTailwind.RunOption {
-    /**
-     Returns the flags to pass to the Tailwind CLI when invoking the `init` command.
-     */
+    /// Returns the flags to pass to the Tailwind CLI when invoking the `init` command.
     var executableFlags: [String] {
         return self.map(\.flag).flatMap({$0})
     }
@@ -102,29 +86,16 @@ extension Set where Element == SwiftyTailwind.RunOption {
 
 public extension SwiftyTailwind {
     enum InitializeOption: Hashable {
-        /**
-         Initializes configuration file as ESM. When passed, it passes the `--esm` flag to the `init` command.
-         */
+        /// Initializes configuration file as ESM. When passed, it passes the `--esm` flag to the `init` command.
         case esm
-        
-        /**
-         Initializes configuration file as Typescript. When passed, it passes the `--ts` flag to the `init` command.
-         */
+        /// Initializes configuration file as Typescript. When passed, it passes the `--ts` flag to the `init` command.
         case ts
-        
-        /**
-         Initializes a `postcss.config.js` file. When passed, it passes the `--postcss` flag to the `init` command.
-         */
+        /// Initializes a `postcss.config.js` file. When passed, it passes the `--postcss` flag to the `init` command.
         case postcss
-        
-        /**
-         Includes the default values for all options in the generated configuration file. When passed, it passes the `--full` flag to the `init` command.
-         */
+        /// Includes the default values for all options in the generated configuration file.
+        /// When passed, it passes the `--full` flag to the `init` command.
         case full
-        
-        /**
-         The CLI flag that represents the option.
-         */
+        /// The CLI flag that represents the option.
         var flag: String {
             switch self {
             case .esm: return "--esm"
@@ -134,48 +105,27 @@ public extension SwiftyTailwind {
             }
         }
     }
-    
-    /**
-     An enum that captures all the options that that you can pass to the Tailwind executable.
-     */
+
+    // An enum that captures all the options that that you can pass to the Tailwind executable.
     enum RunOption: Hashable {
-        /**
-         Keeps the process running watching for file changes. When passed, it passes the `--watch` argument to the Tailwind executable.
-         */
+        /// Keeps the process running watching for file changes. When passed, it passes the `--watch` argument to the Tailwind executable.
         case watch
-        
-        /**
-         It uses polling to watch file changes. When passed, it passes the `--poll` argument to the Tailwind executable.
-         */
+        /// It uses polling to watch file changes. When passed, it passes the `--poll` argument to the Tailwind executable.
         case poll
-        
-        /**
-         It enables [auto-prefixer](https://github.com/postcss/autoprefixer). When passed, it doesn't pass the `--no-autoprefixer` variable.
-         */
+        /// It enables [auto-prefixer](https://github.com/postcss/autoprefixer). When passed, it doesn't pass the `--no-autoprefixer` variable.
         case autoPrefixer
-        
-        /**
-         It  minifies the generated output CSS. When passed, it passes the `--minify` argument to the Tailwind executable.
-         */
+        ///It  minifies the generated output CSS. When passed, it passes the `--minify` argument to the Tailwind executable.
         case minify
-        /**
-         It uses a configuration other than the one in the current working directory. When passed, it passes the `--config` argument to the Tailwind executable.
-         */
+        ///It uses a configuration other than the one in the current working directory. When passed, it passes the `--config` argument to the Tailwind executable.
         case config(AbsolutePath)
-        
-        /**
-         It runs PostCSS using the configuration file at the given path. When passed, it passes the `--postcss` argument to the Tailwind executable.
-         */
+        ///It runs PostCSS using the configuration file at the given path. When passed, it passes the `--postcss` argument to the Tailwind executable.
         case postcss(AbsolutePath)
-        
-        /**
-         It specifies a [glob](https://en.wikipedia.org/wiki/Glob_(programming)) pattern that the Tailwind executable uses to to tree-shake the output CSS eliminating the Tailwind classes that are not used. When passed, it passes the `--content` argument to the Tailwind executable.
-         */
+        ///It specifies a [glob](https://en.wikipedia.org/wiki/Glob_(programming))
+        ///pattern that the Tailwind executable uses to to tree-shake the output CSS
+        /// eliminating the Tailwind classes that are not used.
+        /// When passed, it passes the `--content` argument to the Tailwind executable.
         case content(String)
-        
-        /**
-         The CLI flag that represents the option.
-         */
+        ///The CLI flag that represents the option.
         var flag: [String] {
             switch self {
             case .watch:
