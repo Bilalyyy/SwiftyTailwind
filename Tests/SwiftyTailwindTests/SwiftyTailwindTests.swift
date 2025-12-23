9d727cb4e54ec033b87106fde84843fe0cf07c82
Bilal <yyybilal@gmail.com>
2025-12-23 17:22:52 +0100

    No-op: Adds temporary directory initialisation
    Initialises the temporary directory before usage.
    
    This is a no-op commit.

--- Tests/SwiftyTailwindTests/SwiftyTailwindTests.swift
+++ Tests/SwiftyTailwindTests/SwiftyTailwindTests.swift
@@ -3,25 +3,27 @@ import TSCBasic
 @testable import SwiftyTailwind
 
 final class SwiftyTailwindTests: XCTestCase {
+    // TODO: These tests failed
+
     func test_initialize() async throws {
         try await withTemporaryDirectory(removeTreeOnDeinit: true, { tmpDir in
             // Given
             let subject = SwiftyTailwind(directory: tmpDir)
-            
+
             // When
             try await subject.initialize(directory: tmpDir, options: .full)
-            
+
             // Then
             let tailwindConfigPath = tmpDir.appending(component: "tailwind.config.js")
             XCTAssertTrue(localFileSystem.exists(tailwindConfigPath))
         })
     }
-    
+
     func test_run() async throws {
         try await withTemporaryDirectory(removeTreeOnDeinit: true, { tmpDir in
             // Given
             let subject = SwiftyTailwind(directory: tmpDir)
-            
+
             let inputCSSPath = tmpDir.appending(component: "input.css")
             let inputCSSContent = """
             @tailwind components;
@@ -31,12 +33,12 @@ final class SwiftyTailwindTests: XCTestCase {
             }
             """
             let outputCSSPath = tmpDir.appending(component: "output.css")
-            
+
             try localFileSystem.writeFileContents(inputCSSPath, bytes: ByteString(inputCSSContent.utf8))
-            
+
             // When
             try await subject.run(input: inputCSSPath, output: outputCSSPath, directory: tmpDir)
-            
+
             // Then
             let content = String(bytes: try localFileSystem.readFileContents(outputCSSPath).contents, encoding: .utf8)
             XCTAssertTrue(localFileSystem.exists(outputCSSPath))
