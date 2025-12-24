1186d38016b47bcfa89d796358bed836345e8a83
Bilal Larose <93541524+Bilalyyy@users.noreply.github.com>
2025-12-24 10:45:34 +0100

    Fix tailwind test content generation

--- Tests/SwiftyTailwindTests/SwiftyTailwindTests.swift
+++ Tests/SwiftyTailwindTests/SwiftyTailwindTests.swift
@@ -12,22 +12,27 @@ final class SwiftyTailwindTests: XCTestCase {
 
             let inputCSSPath = tmpDir.appending(component: "input.css")
             let inputCSSContent = """
-            @tailwind components;
-            
-            p {
-                @apply font-bold;
-            }
+            @tailwind utilities;
             """
             let outputCSSPath = tmpDir.appending(component: "output.css")
+            let contentPath = tmpDir.appending(component: "index.html")
+            let contentHTML = """
+            <p class="font-bold">Hello</p>
+            """
 
             try localFileSystem.writeFileContents(inputCSSPath, bytes: ByteString(inputCSSContent.utf8))
+            try localFileSystem.writeFileContents(contentPath, bytes: ByteString(contentHTML.utf8))
 
             // When
-            try await subject.run(input: inputCSSPath, output: outputCSSPath, directory: tmpDir)
+            try await subject.run(input: inputCSSPath,
+                                  output: outputCSSPath,
+                                  directory: tmpDir,
+                                  options: .content(contentPath.pathString))
 
             // Then
             let content = String(bytes: try localFileSystem.readFileContents(outputCSSPath).contents, encoding: .utf8)
             XCTAssertTrue(localFileSystem.exists(outputCSSPath))
+            XCTAssertTrue(content?.contains(".font-bold") != nil)
             XCTAssertTrue(content?.contains("font-weight: 700") != nil)
         })
     }
